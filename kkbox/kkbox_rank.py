cd377276d378438b2fe07355ff68dfaa1015f5c3
friday27 <yiyinghahalin@gmail.com>
2018-03-08 00:05:42 -0800

    move kkbox-related files to /kkbox

--- kkbox/kkbox_rank.py
+++ kkbox/kkbox_rank.py
@@ -1,4 +1,12 @@
 import os
+import re
+
+def remove_punctuation(line):
+	rule_zh = re.compile(u"[^0-9\u4e00-\u9fa5]")  # [^a-zA-Z0-9\u4e00-\u9fa5]
+	line_zh = rule_zh.sub('',line)
+	rule_en = re.compile(u"[^a-zA-Z0-9]")
+	line_en = rule_en.sub('',line)
+	return line_zh, line_en
 
 for dirs, _, filenames in os.walk("./kkbox_tw/"):
 	txt_files = ["./kkbox_tw/"+x for x in filenames if x[-4:] == ".txt"]
@@ -13,21 +21,24 @@ for txt in txt_files:
 		for rank, pair in enumerate(pairs):
 			pair = pair.split("\t")
 			song = pair[0]
+			
 			singer_1 = pair[1]
 			singer_2 = pair[2] if len(pair) > 2 else ""
+			singer_1_zh, singer_1_en = remove_punctuation(singer_1)
+			singer_2_zh, singer_2_en = remove_punctuation(singer_2)
 
 			try:
 				song_dict[song] += (rank+1)
 			except KeyError:
 				song_dict[song] = (rank+1)
 
-			pair_dict[song] = [singer_1, singer_2]
+			pair_dict[song] = [singer_1_zh, singer_1_en, singer_2_zh, singer_2_en]
 
 print("Songs: ", len(song_dict))
 
 sorted_songs = sorted(song_dict, key=song_dict.get, reverse=False)
 with open("./kkbox_songs_tw.txt", "w") as f:
 	for song in sorted_songs:
-		line = "{}\t{}\t{}".format(song, pair_dict[song][0], pair_dict[song][1])
+		line = "{}\t{}\t{}\t{}\t{}".format(song, pair_dict[song][0], pair_dict[song][1], pair_dict[song][2], pair_dict[song][3])
 		# print(line)
 		f.write(line)
